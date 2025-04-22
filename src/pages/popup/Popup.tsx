2aaae6fc1a7eb4e57927d50a7a99999587a4838b
Luke Bullen <luke@MacBook-Pro.local>
2025-04-22 23:24:24 +0100

    feat(popup): establish popup layout

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from '@assets/img/logo.svg';
-import {Header} from '@pages/popup/components';
+import { Header, Input, SavedList } from '@pages/popup/components';
+import { Stack } from '@mui/material';
 
 export default function Popup() {
   return (
-    <>
-    <Header />
-    </>
+      <Stack p="1rem" width="100%" height="100%">
+        <Header />
+        <Input />
+        <SavedList />
+      </Stack>
   );
 }
