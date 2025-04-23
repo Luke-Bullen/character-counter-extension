74233fe50a362321267c97d5d7c00f74818ea70b
Luke-Bullen <lukeabullen@gmail.com>
2025-04-23 23:58:45 +0100

    feat(input): add basic input field

--- src/pages/popup/components/Input.tsx
+++ src/pages/popup/components/Input.tsx
@@ -1,5 +1,24 @@
+import { Stack, TextField } from '@mui/material'
+import { useState } from 'react'
+
 const Input = () => {
-    return (<p>Input</p>)
+  const [inputValue, setInputValue] = useState('')
+
+  return (
+    <>
+      <Stack>
+        <TextField
+          variant='outlined'
+          multiline
+          maxRows={4}
+          sx={{ background: 'white', width: '95%' }}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          slotProps={{ htmlInput: { maxLength: 5000 } }}
+        />
+      </Stack>
+    </>
+  )
 }
 
 export default Input
