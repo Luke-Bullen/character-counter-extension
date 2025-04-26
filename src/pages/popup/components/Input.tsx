1aa3f696115ed408c387f77e2a338196064ae1f1
Luke-Bullen <lukeabullen@gmail.com>
2025-04-26 17:03:50 +0100

    chore(input): test husky

--- src/pages/popup/components/Input.tsx
+++ src/pages/popup/components/Input.tsx
@@ -1,56 +1,56 @@
-import { Stack, TextField, Typography } from '@mui/material'
-import { FC, useState } from 'react'
-import { useByteCount, useCharacterCount } from '../hooks'
+import { Stack, TextField, Typography } from "@mui/material";
+import { FC, useState } from "react";
+import { useByteCount, useCharacterCount } from "../hooks";
 
 const InputValues: FC<{ inputValue: string }> = ({ inputValue }) => {
-  const inputCharacterCountValue = useCharacterCount(inputValue)
-  const inputByteCountValue = useByteCount(inputValue)
+  const inputCharacterCountValue = useCharacterCount(inputValue);
+  const inputByteCountValue = useByteCount(inputValue);
 
   return (
-    <Stack direction='column' alignItems='flex-end'>
-      <Typography variant='body1'>
+    <Stack direction="column" alignItems="flex-end">
+      <Typography variant="body1">
         Characters: {inputCharacterCountValue}
       </Typography>
-      <Typography variant='body1'>Bytes: {inputByteCountValue}</Typography>
+      <Typography variant="body1">Bytes: {inputByteCountValue}</Typography>
     </Stack>
-  )
-}
+  );
+};
 
 const Actions: FC = () => {
   return (
-    <Stack direction='row'>
+    <Stack direction="row">
       <p>copy</p>
       <p>save</p>
     </Stack>
-  )
-}
+  );
+};
 
 const Input: FC = () => {
-  const [inputValue, setInputValue] = useState('')
+  const [inputValue, setInputValue] = useState("");
 
   return (
     <>
       <Stack>
         <TextField
-          variant='outlined'
+          variant="outlined"
           multiline
           maxRows={4}
-          sx={{ background: 'white', width: '95%' }}
+          sx={{ background: "white", width: "95%" }}
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
           slotProps={{ htmlInput: { maxLength: 5000 } }}
         />
         <Stack
-          direction='row'
-          justifyContent='space-between'
-          alignItems='center'
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
         >
           <InputValues inputValue={inputValue} />
           <Actions />
         </Stack>
       </Stack>
     </>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
