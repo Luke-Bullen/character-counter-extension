02d3d22374d9caf4511b06c5dd2466a2f6dbd8a8
Luke-Bullen <lukeabullen@gmail.com>
2025-04-27 23:27:24 +0100

    feat(tooltip): add props

--- src/pages/popup/features/shared/Tooltip.tsx
+++ src/pages/popup/features/shared/Tooltip.tsx
@@ -1,5 +1,11 @@
-const Tooltip = () => {
-  return <p>Tooltip</p>;
+import { Tooltip as MUITooltip } from '@mui/material';
+import { FC, ReactElement } from 'react';
+
+const Tooltip: FC<{ title: string; children: ReactElement }> = ({
+  title,
+  children,
+}) => {
+  return <MUITooltip title={title}>{children}</MUITooltip>;
 };
 
 export default Tooltip;
