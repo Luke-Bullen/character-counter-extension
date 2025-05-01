1f2f980ce1414af2aa62ee5c4836388137efcee0
Luke-Bullen <lukeabullen@gmail.com>
2025-05-01 23:51:57 +0100

    feat(redux): add setup function

--- src/pages/popup/redux/store.ts
+++ src/pages/popup/redux/store.ts
@@ -1,11 +1,21 @@
+import browser from 'webextension-polyfill';
 import { configureStore } from '@reduxjs/toolkit';
-import listSlice from './listSlice';
+import listSlice, { setOrder, setSavedEntries } from './listSlice';
 
 const store = configureStore({
   reducer: {
     list: listSlice,
   },
 });
+
+const setup = async () => {
+  const { order, ...rest }: { order?: string[] } =
+    await browser.storage.local.get(null);
+  if (order) store.dispatch(setOrder(order));
+  store.dispatch(setSavedEntries(rest));
+};
+
+setup();
 
 store.subscribe(() => {
   console.log('store sub', store.getState().list);
