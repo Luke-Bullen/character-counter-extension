bda7522f01ada4c39550c1d4cc2637203d8fc39f
Luke-Bullen <lukeabullen@gmail.com>
2025-05-07 23:52:56 +0100

    feat(savedlist): add idea of getting entities from storage

--- src/pages/popup/features/savedList/SavedList.tsx
+++ src/pages/popup/features/savedList/SavedList.tsx
@@ -1,16 +1,44 @@
 import { Stack } from '@mui/material';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import SavedListItem from './SavedListItem';
+import { EntityObjectType } from '../shared';
+import { store } from '../../redux';
 
 const SavedList: FC = () => {
   const tempOrderArray = ['1', '2', '3'];
 
+  const [orderArray, setOrderArray] = useState<string[]>([]);
+  const [allSavedEntriesObject, setAllSavedEntriesObject] = useState<
+    Record<string, EntityObjectType>
+  >({});
+
+  const getAllSavedEntries = async () => {
+    const { listOrder, savedEntries } = store.getState().list;
+
+    setOrderArray(listOrder);
+    setAllSavedEntriesObject(savedEntries);
+  };
+
+  useEffect(() => {
+    getAllSavedEntries();
+  }, []);
+  store.subscribe(getAllSavedEntries);
+
+  console.log(orderArray);
+  console.log(allSavedEntriesObject);
+
   return (
-    <Stack direction='column'>
-      {tempOrderArray.map((id) => {
-        return <SavedListItem key={id} id={id} />;
-      })}
-    </Stack>
+    <>
+      {orderArray.length ? (
+        <Stack direction='column'>
+          {tempOrderArray.map((id) => {
+            return <SavedListItem key={id} id={id} />;
+          })}
+        </Stack>
+      ) : (
+        <p>There are no saved entries</p>
+      )}
+    </>
   );
 };
 
