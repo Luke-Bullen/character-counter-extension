274f6d82bcd299833d5bd8b606120fe68a3b2445
Luke-Bullen <lukeabullen@gmail.com>
2025-05-09 00:05:01 +0100

    style(savedlistitem): convert to styled component

--- src/pages/popup/features/savedList/SavedListItem.tsx
+++ src/pages/popup/features/savedList/SavedListItem.tsx
@@ -3,6 +3,7 @@ import {
   AccordionDetails,
   AccordionSummary,
   Stack,
+  styled,
   Typography,
 } from '@mui/material';
 import { ExpandMore } from '@mui/icons-material';
@@ -14,6 +15,13 @@ const SavedListItem: FC<{ id: string; entityProperties: EntityObjectType }> = ({
   entityProperties,
 }) => {
   const { alias, value, characterCount, byteCount } = entityProperties;
+
+  const ValueTypography = styled(Typography)({
+    fontWeight: 'bold',
+    width: '90%',
+    overflow: 'scroll',
+    height: '40px',
+  });
 
   return (
     <Accordion key={id} id={`${alias}-key`}>
@@ -40,17 +48,7 @@ const SavedListItem: FC<{ id: string; entityProperties: EntityObjectType }> = ({
       </AccordionSummary>
       <AccordionDetails>
         <Stack direction='column' alignItems='center' spacing={1}>
-          <Typography
-            variant='body1'
-            sx={{
-              fontWeight: 'bold',
-              width: '90%',
-              overflow: 'scroll',
-              height: '40px',
-            }}
-          >
-            {value}
-          </Typography>
+          <ValueTypography variant='body1'>{value}</ValueTypography>
           <Stack direction='column' alignItems='flex-start' width='100%'>
             <Typography variant='body2'>Charcters: {characterCount}</Typography>
             <Typography variant='body2'>Bytes: {byteCount}</Typography>
