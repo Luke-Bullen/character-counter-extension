ba18735ebf8d58db0bfa590112febc0eae9d0265
Luke-Bullen <lukeabullen@gmail.com>
2025-05-11 23:40:19 +0100

    style(popup): add gap between core components

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -4,7 +4,7 @@ import { Stack } from '@mui/material';
 
 export default function Popup() {
   return (
-    <Stack p="1rem" width="100%" height="100%">
+    <Stack p='1rem' width='100%' height='100%' gap='0.5rem'>
       <Header />
       <Input />
       <SavedList />
