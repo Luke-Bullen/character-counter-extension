281cd930d46e1b9a19f96ba1edb15efea02857a0
Luke-Bullen <lukeabullen@gmail.com>
2025-05-22 23:52:49 +0100

    refactor(header): remove unneeded prop

--- src/pages/popup/features/header/Header.tsx
+++ src/pages/popup/features/header/Header.tsx
@@ -3,9 +3,7 @@ import { Stack, Switch, Typography } from '@mui/material';
 const Header = () => {
   return (
     <Stack direction='row' justifyContent='space-between' alignItems='center'>
-      <Typography variant='h5' color='black'>
-        Character Counter
-      </Typography>
+      <Typography variant='h5'>Character Counter</Typography>
       <Switch />
     </Stack>
   );
