58699e6452a505ec3b18b203ee48d0b05e1756e2
Luke-Bullen <lukeabullen@gmail.com>
2025-05-24 23:49:55 +0100

    refactor(header): move switch to own component

--- src/pages/popup/features/header/Header.tsx
+++ src/pages/popup/features/header/Header.tsx
@@ -1,10 +1,11 @@
-import { Stack, Switch, Typography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
+import ThemeSwitch from './ThemeSwitch';
 
 const Header = () => {
   return (
     <Stack direction='row' justifyContent='space-between' alignItems='center'>
       <Typography variant='h5'>Character Counter</Typography>
-      <Switch />
+      <ThemeSwitch />
     </Stack>
   );
 };
---
+++ src/pages/popup/features/header/ThemeSwitch.tsx
@@ -0,0 +1,7 @@
+import { Switch } from '@mui/material';
+
+const ThemeSwitch = () => {
+  return <Switch />;
+};
+
+export default ThemeSwitch;
