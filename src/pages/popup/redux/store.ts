00df21c09a7bb157fe260548dcedaf6a0d9b745d
Luke-Bullen <lukeabullen@gmail.com>
2025-05-25 00:16:39 +0100

    feat(theme): add theme to local storage

--- src/pages/popup/redux/store.ts
+++ src/pages/popup/redux/store.ts
@@ -1,28 +1,40 @@
 import browser from 'webextension-polyfill';
 import { configureStore } from '@reduxjs/toolkit';
 import listSlice, { setOrder, setSavedEntries } from './listSlice';
+import themeSlice, { setTheme } from './themeSlice';
 
 const store = configureStore({
   reducer: {
     list: listSlice,
+    theme: themeSlice,
   },
 });
 
 const setup = async () => {
-  const { order, ...rest }: { order?: string[] } =
+  const {
+    order,
+    darkTheme,
+    ...rest
+  }: { order?: string[]; darkTheme?: boolean } =
     await browser.storage.local.get(null);
   if (order) store.dispatch(setOrder(order));
+  if (darkTheme) store.dispatch(setTheme(darkTheme));
   store.dispatch(setSavedEntries(rest));
 };
 
 setup();
 
 store.subscribe(async () => {
-  const { listOrder, savedEntries } = store.getState().list;
+  const {
+    list: { listOrder, savedEntries },
+    theme: { darkTheme },
+  } = store.getState();
 
   await browser.storage.local.set({ order: listOrder });
 
   await browser.storage.local.set(savedEntries);
+
+  await browser.storage.local.set({ darkTheme });
 });
 
 export default store;
