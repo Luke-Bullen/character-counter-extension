37e53464abd5048628c2b266884f20992c793cdd
Luke-Bullen <lukeabullen@gmail.com>
2025-05-25 00:12:13 +0100

    feat(theme): add setTheme reducer

--- src/pages/popup/redux/themeSlice.ts
+++ src/pages/popup/redux/themeSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 type ThemeType = {
   darkTheme: boolean;
@@ -12,11 +12,14 @@ const themeSlice = createSlice({
   name: 'themeSlice',
   initialState,
   reducers: {
+    setTheme: (state, action: PayloadAction<boolean>) => {
+      state.darkTheme = action.payload;
+    },
     switchTheme: (state) => {
       state.darkTheme = !state.darkTheme;
     },
   },
 });
 
-export const { switchTheme } = themeSlice.actions;
+export const { setTheme, switchTheme } = themeSlice.actions;
 export default themeSlice.reducer;
