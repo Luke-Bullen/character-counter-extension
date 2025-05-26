65269defbca066725e5ec312e6977f27ab5b9f3a
Luke-Bullen <lukeabullen@gmail.com>
2025-05-26 20:55:43 +0100

    feat(theme): switch controlled by darkTheme state

--- src/pages/popup/features/header/ThemeSwitch.tsx
+++ src/pages/popup/features/header/ThemeSwitch.tsx
@@ -1,7 +1,18 @@
 import { Switch } from '@mui/material';
+import { store, switchTheme } from '../../redux';
+import { useState } from 'react';
 
 const ThemeSwitch = () => {
-  return <Switch />;
+  const [isDarkTheme, setIsDarkTheme] = useState<boolean>(
+    store.getState().theme.darkTheme,
+  );
+
+  const handleClick = () => {
+    store.dispatch(switchTheme());
+    setIsDarkTheme(store.getState().theme.darkTheme);
+  };
+
+  return <Switch checked={isDarkTheme} onChange={handleClick} />;
 };
 
 export default ThemeSwitch;
