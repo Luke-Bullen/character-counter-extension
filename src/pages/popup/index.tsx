a545b31c0d777f7db63c886998e86278840da73f
Luke-Bullen <lukeabullen@gmail.com>
2025-05-28 23:55:29 +0100

    feat: add react strict mode

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import '@pages/popup/index.css';
 import '@assets/styles/tailwind.css';
 import Popup from '@pages/popup/Popup';
 
 function init() {
-	const rootContainer = document.querySelector('#__root');
-	if (!rootContainer) throw new Error("Can't find Popup root element");
-	const root = createRoot(rootContainer);
-	root.render(<Popup />);
+  const rootContainer = document.querySelector('#__root');
+  if (!rootContainer) throw new Error("Can't find Popup root element");
+  const root = createRoot(rootContainer);
+  root.render(
+    <StrictMode>
+      <Popup />
+    </StrictMode>,
+  );
 }
 
 init();
